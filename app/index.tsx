1da1ad70c83e26fe9d013ae800ddcbb533b1331e
Srj <soorajp2002@gmail.com>
2025-07-24 00:26:41 +0530

    nativewind config fix

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  )
+}
+
+export default index
